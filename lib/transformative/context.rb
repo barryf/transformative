2b8e6bf9ae3cfe3581e86f8afb41f47ce04fd468
Barry Frost <public@barryfrost.com>
2016-11-03 08:48:28 +0000

    Expose twitter_client.

--- lib/transformative/context.rb
+++ lib/transformative/context.rb
@@ -118,8 +118,6 @@ module Transformative
       end
     end
 
-    private
-
     def twitter_client
       @twitter_client ||= Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
