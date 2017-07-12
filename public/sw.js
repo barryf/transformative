7075e9b180fba9ec0063c62fb6f8a79ba38fa715
Barry Frost <public@barryfrost.com>
2017-07-12 17:03:20 +0100

    Tentative attempt at becoming a PWA.

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+ e.waitUntil(
+   // after the service worker is installed,
+   // open a new cache
+   caches.open('barryfrost-com-cache').then(cache => {
+     // add all URLs of resources we want to cache
+     return cache.addAll([
+       '/',
+       '/2017/06/promulgate-wip'
+     ]);
+   })
+ );
+});
