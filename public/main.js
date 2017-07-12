e71bec7dd32b42d68d2db038ef7730bc3c5b9a8b
Barry Frost <public@barryfrost.com>
2017-07-12 17:11:27 +0100

    Remove SW function wrapper.

--- public/main.js
+++ public/main.js
@@ -1,10 +1,8 @@
-function registerServiceWorker() {
-  // register sw script in supporting browsers
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(() => {
-      console.log('Service Worker registered successfully.');
-    }).catch(error => {
-      console.log('Service Worker registration failed:', error);
-    });
-  }
+// register sw script in supporting browsers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(() => {
+    console.log('Service Worker registered successfully.');
+  }).catch(error => {
+    console.log('Service Worker registration failed:', error);
+  });
 }
